ec64a6776e158180cd3a1e7aaa7727f01072b2b0
Amal Prasad Trivedi <142908194+amalprasadtrivedi@users.noreply.github.com>
2025-02-26 11:06:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ st.set_page_config(page_title="Health Assistant",
                    layout="wide",
                    page_icon="🧑‍⚕️")
 
-# getting the working directory of the main.py
-working_dir = os.path.dirname(os.path.abspath(__file__))
-
 # loading the saved models
 
-diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('/saved_models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('/saved_models/heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('/saved_models/parkinsons_model.sav', 'rb'))
 
 # Sidebar
 st.sidebar.title("S.O.P.H.I.E. Health Assistant")
